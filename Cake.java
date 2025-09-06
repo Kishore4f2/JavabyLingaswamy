4218b6d0b9681dd34708b9afb69b278db389de9b
Kishore4f2 <satyakishore273@gmail.com>
2025-09-06 16:40:12 +0530

    constructor

---
+++ Cake.java
@@ -0,0 +1,14 @@
+class Cake {
+    String name;
+    String flavour;
+    String shape;
+    int kgs;
+    double price;
+    Cake(String n, String f, String s, int kg, double p) {
+        name =n;
+        flavour = f;
+        shape = s;
+        kgs = kg;
+        price = p;
+    }
+}
---
+++ friend.java
@@ -0,0 +1,11 @@
+public class friend {
+    public static void main(String[] args) {
+        Cake c = new Cake("Vinay","Chocolate","Round",2,500.0);
+        System.out.println(c.name);
+        System.out.println(c.flavour);
+        System.out.println(c.shape);
+        System.out.println(c.kgs);
+        System.out.println(c.price);
+    }
+    
+}
