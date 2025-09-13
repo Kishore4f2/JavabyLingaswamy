553098ffffaeab227d7c8c02ad78e71d57f137ce
Kishore4f2 <satyakishore273@gmail.com>
2025-09-13 18:10:20 +0530

    arraystrings

---
+++ ArrSec.java
@@ -0,0 +1,27 @@
+public class ArrSec {
+
+	public static void main(String[] args) {
+		
+		String[] arr;
+		
+		arr = new String[4];
+		
+		arr[0] = "Satya Narayana";
+		arr[1] = "Devi";
+		arr[2] = "Satya Kishore";
+		arr[3] = "Keerthana";
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		
+		System.out.println();
+		
+		for(int i=0;i<4;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
